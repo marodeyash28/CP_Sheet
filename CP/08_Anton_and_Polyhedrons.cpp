eebf44c370e08a758a5a6fdd85e18502a4fa2306
Yash Marode <123548505+marodeyash28@users.noreply.github.com>
2024-02-07 17:57:49 +0530

    Add files via upload

---
+++ CP/08_Anton_and_Polyhedrons.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin>>n;
+  int sum=0; 
+  while(n--){
+    string s;
+    cin>>s;
+    
+    if(s == "Tetrahedron") sum=sum+4;
+    else if(s== "Cube") sum=sum+6;
+    else if(s== "Octahedron") sum=sum+8;
+    else if(s== "Dodecahedron") sum=sum+12;
+    else sum=sum+20;
+  }
+  cout<<sum;
+}
